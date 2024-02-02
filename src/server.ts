127c230d07304f692faa7fab190efb376526ba72
Flandre <flantan233@gmail.com>
2024-02-02 17:04:34 +0800

    change public dir

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ const app = new Koa();
 const PORT = 3000;
 
 // 配置静态文件服务
-app.use(serve(path.join(__dirname, '/output')));
+app.use(serve(path.join(__dirname, '..', '/output')));
 
 app.listen(PORT, () => {
   console.log(`[Koa Server] http://localhost:${PORT}`);
