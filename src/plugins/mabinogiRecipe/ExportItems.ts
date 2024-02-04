cea68b268b7c352b1f1c20f4018b944a59e80cd6
Flandre <flantan233@gmail.com>
2024-02-04 14:41:29 +0800

    fix import source js

--- src/plugins/mabinogiRecipe/ExportItems.ts
+++ src/plugins/mabinogiRecipe/ExportItems.ts
@@ -1,45 +1,46 @@
 import fs from "fs-extra";
 import path from "node:path";
+const iconv = require('iconv-lite');
 
-eval(fs.readFileSync(path.join(__dirname, '/source/js/Skill.js')).toString('utf-16le'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/TailoringItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/MillingItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/HandicraftItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/WeavingItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/RefineItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/BlacksmithItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/CookingItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/PotionMakingItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/FishingItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/DissolutionItem.js')).toString('utf-16le'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/SynthesisItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/CarpentryItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/StageTicketMakingItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/HeulwenEngineeringItem.js')).toString('utf-16le'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/MagicCraftItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/StationaryCraftItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/FynnsCraftItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/ManaFormingItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/MetalConversionItem.js')).toString('utf-16le'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/CommerceMasteryProduct.js')).toString('utf-16le'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/RelicInvestigationItem.js')).toString('utf-16le'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/ErgEnhance.js')).toString('utf-16le'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/Item.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/ErinnFormula..js')).toString('utf-8')) // 需要屏蔽最後加載NPCTime的timer
-eval(fs.readFileSync(path.join(__dirname, '/source/js/KillItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/ExplorationItem.js')).toString('utf-16le'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/GiftItem.js')).toString('utf-16le'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/QuestItem.js')).toString('utf-8'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/ErinnWeaponShapeTransformScroll..js')).toString('utf-16le'))
-eval(fs.readFileSync(path.join(__dirname, '/source/js/Product.js')).toString('utf-8'))
-
+// 在全局作用域中访问 SkillList
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/Skill.js')).toString('utf-16le'));
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/TailoringItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/MillingItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/HandicraftItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/WeavingItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/RefineItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/BlacksmithItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/CookingItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/PotionMakingItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/FishingItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/DissolutionItem.js')).toString('utf-16le'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/SynthesisItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/CarpentryItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/StageTicketMakingItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/HeulwenEngineeringItem.js')).toString('utf-16le'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/MagicCraftItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/StationaryCraftItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/FynnsCraftItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/ManaFormingItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/MetalConversionItem.js')).toString('utf-16le'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/CommerceMasteryProduct.js')).toString('utf-16le'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/RelicInvestigationItem.js')).toString('utf-16le'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/ErgEnhance.js')).toString('utf-16le'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/Item.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/ErinnFormula..js')).toString('utf-8')) // 需要屏蔽最後加載NPCTime的timer
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/KillItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/ExplorationItem.js')).toString('utf-16le'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/GiftItem.js')).toString('utf-16le'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/QuestItem.js')).toString('utf-8'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/ErinnWeaponShapeTransformScroll..js')).toString('utf-16le'))
+eval.call(global, fs.readFileSync(path.join(__dirname, '/source/js/Product.js')).toString('utf-8'))
 
 const ItemNameToItemId = new Map()
 const ItemIdToItemDetail = new Map()
 
 const CreateSkillLists = () => {
   //@ts-ignore
-  SkillList.forEach(skillId => {
+  (global as any).SkillList.forEach(skillId => {
     let skill = eval(`Skill${skillId}`)
     console.log(`===== ${skillId} ${skill[0]} =====`)
     let skillList = eval(`${skill[1]}List`)
@@ -79,6 +80,37 @@ const FormatItems = (skillList: any, skillName: string, skillCode: string | numb
 
 const getItems = () => {
   if (!ItemNameToItemId.size) {
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/Skill.js')).toString('utf-16le'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/TailoringItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/MillingItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/HandicraftItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/WeavingItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/RefineItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/BlacksmithItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/CookingItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/PotionMakingItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/FishingItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/DissolutionItem.js')).toString('utf-16le'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/SynthesisItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/CarpentryItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/StageTicketMakingItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/HeulwenEngineeringItem.js')).toString('utf-16le'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/MagicCraftItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/StationaryCraftItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/FynnsCraftItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/ManaFormingItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/MetalConversionItem.js')).toString('utf-16le'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/CommerceMasteryProduct.js')).toString('utf-16le'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/RelicInvestigationItem.js')).toString('utf-16le'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/ErgEnhance.js')).toString('utf-16le'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/Item.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/ErinnFormula..js')).toString('utf-8')) // 需要屏蔽最後加載NPCTime的timer
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/KillItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/ExplorationItem.js')).toString('utf-16le'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/GiftItem.js')).toString('utf-16le'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/QuestItem.js')).toString('utf-8'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/ErinnWeaponShapeTransformScroll..js')).toString('utf-16le'))
+    eval(fs.readFileSync(path.join(__dirname, '/source/js/Product.js')).toString('utf-8'))
     CreateSkillLists()
   }
   return {
